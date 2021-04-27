b955f13b0da707d8fc21145467b2ed0d7b7b32e7
Diego Salgado <Djsalgado14@gmail.com>
2021-04-27 10:17:20 -0700

    made it run

--- webapp.py
+++ webapp.py
@@ -28,14 +28,15 @@ def renderPage1():
 
 @app.route('/page_2',methods=['GET','POST'])
 def renderPage2():
-    session["pet"]=request.form['pets']
-
+    if("pet" not in session): 
+	    session["pet"]=request.form['pet']
+	
     return render_template('page2.html')
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
-    session["favoriteColor"]=request.form['favoriteColor']
+    session["dessert"]=request.form['dessert']
     return render_template('page3.html')
     
 if __name__=="__main__":
-    app.run(debug=False)
+    app.run(debug=True)
